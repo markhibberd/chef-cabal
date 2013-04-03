9c76ba60afa63d84bfb66b63420412ec289da1dd
markhibberd <mark@hibberd.id.au>
2013-04-03 23:08:46 +1100

    Extracted to own repo.:

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+## 0.1.1 (April 3, 2013)
+
+* Doc fixes only.
+
+## 0.1.0 (April 3, 2013)
+
+* Initial release of cabal.
+
+[@markhibberd]: https://github.com/apiengine/cabal
---
+++ providers/update.rb
@@ -0,0 +1,19 @@
+#
+# Cookbook Name:: cabal
+# Provider:: cabal_update
+#
+
+def whyrun_supported?
+  true
+end
+
+action :update do
+  index = ::File.expand_path("~#{ new_resource.user }/.cabal/packages/hackage.haskell.org/00-index.cache")
+  age = (Time.now - ::File.mtime(index)) / 60
+
+  if age > new_resource.cache_for
+    converge_by("cabal update #{ new_resource.user }") do
+      execute "su - #{ new_resource.user } -c 'cabal update'"
+    end
+  end
+end
