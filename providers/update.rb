09eb7141bd94a88c4e6662109e5181e505e43e50
markhibberd <mark@hibberd.id.au>
2013-04-17 21:52:33 +1000

    Check if index file exists before calling mtime.
    Closes #1.

--- providers/update.rb
+++ providers/update.rb
@@ -9,9 +9,12 @@ end
 
 action :update do
   index = ::File.expand_path("~#{ new_resource.user }/.cabal/packages/hackage.haskell.org/00-index.cache")
-  age = (Time.now - ::File.mtime(index)) / 60
 
-  if age > new_resource.cache_for
+  def age
+    (Time.now - ::File.mtime(index)) / 60
+  end
+
+  if !::File.exists?(index) || age > new_resource.cache_for
     converge_by("cabal update #{ new_resource.user }") do
       execute "su - #{ new_resource.user } -c 'cabal update'"
     end
