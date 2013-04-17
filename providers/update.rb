b2f99bf0f9a3442752787929a5777ef3b28239f2
markhibberd <mark@hibberd.id.au>
2013-04-17 22:23:47 +1000

    Don't try to close over index.
    Closes #1 (really)

--- providers/update.rb
+++ providers/update.rb
@@ -10,11 +10,11 @@ end
 action :update do
   index = ::File.expand_path("~#{ new_resource.user }/.cabal/packages/hackage.haskell.org/00-index.cache")
 
-  def age
-    (Time.now - ::File.mtime(index)) / 60
+  def age(path)
+    (Time.now - ::File.mtime(path)) / 60
   end
 
-  if !::File.exists?(index) || age > new_resource.cache_for
+  if !::File.exists?(index) || age(index) > new_resource.cache_for
     converge_by("cabal update #{ new_resource.user }") do
       execute "su - #{ new_resource.user } -c 'cabal update'"
     end
